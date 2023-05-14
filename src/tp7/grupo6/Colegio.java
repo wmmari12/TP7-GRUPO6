ef65ce5a6f685f80e99a184ff0fc054a805d8a3c
Lucas Fernandez <lucasm2664789183@gmail.com>
2023-05-14 20:15:41 -0300

    sad

--- src/tp7/grupo6/Colegio.java
+++ src/tp7/grupo6/Colegio.java
@@ -7,12 +7,11 @@ package tp7.grupo6;
 
 import java.util.HashSet;
 
-
 public class Colegio {
 
     
     public static void main(String[] args) {
-        
+
         
         Alumno alumno = new Alumno(123, "juan", "juan");
         Materia materia = new Materia(1, "matematica", 1);
