0e6e5607b54975ecc30b170ec01e05c0ee6236cb
wmmari12 <wmari12@gmail.com>
2023-05-22 17:31:20 -0300

    materiaData

--- src/AccesoADatos/MateriaData.java
+++ src/AccesoADatos/MateriaData.java
@@ -1,6 +1,61 @@
 
 package AccesoADatos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import tp7.grupo6.Materia;
+
 public class MateriaData {
+    private Connection con = null;
+
+    public MateriaData() {
+        
+        con = Conexion.getConexion();
+        
+    }
     
+    public void guardarMateria(Materia materia){
+        
+        String sql ="INSERT INTO `materia`(`idMateria`, `nombre`, `año`, `estado`) VALUES (?,?,?,?)";
+        try{
+            PreparedStatement ps=con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, materia.getIdMateria());
+            ps.setString(2, materia.getNombre());
+            ps.setInt(3, materia.getAnio());
+            ps.setBoolean(4, materia.isEstado());
+            ps.executeUpdate();
+            ResultSet rs=ps.getGeneratedKeys();
+            if(rs.next()){
+                JOptionPane.showMessageDialog(null, "Materia cargada con exito");
+            }
+        }catch (SQLException ex)
+        {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Materia" + ex);
+        }
+    }
+    public List<Materia> listarMaterias(){
+        List<Materia> materias=new ArrayList<>();
+        
+        
+        
+        return  materias;
+    }
+    
+    public void buscarMateria(int id){
+        
+    }
+    
+    public void borrarMateria(int id){
+        
+    }
+    
+    public void actualizarMateria(Materia materia){
+        
+    }
 }
--- src/tp7/grupo6/Materia.java
+++ src/tp7/grupo6/Materia.java
@@ -4,18 +4,18 @@ package tp7.grupo6;
 
 public class Materia {
     
+    private int idMateria;
     private String nombre;
     private int anio;
-    private int id_materia;
     private boolean estado;
 
     public Materia() {
     }
 
-    public Materia(String nombre, int anio, int id_materia, boolean estado) {
+    public Materia(int idMateria, String nombre, int anio, boolean estado) {
         this.nombre = nombre;
         this.anio = anio;
-        this.id_materia = id_materia;
+        this.idMateria = idMateria;
         this.estado = estado;
     }
 
@@ -23,6 +23,42 @@ public class Materia {
         this.nombre = nombre;
         this.anio = anio;
         this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public int getIdMateria(String idMateria) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     
